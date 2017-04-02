69917565517757f5de71dd56e56db962d2be18b8
vhy贺悦 <he.y@Ctrip.com>
2017-04-02 20:24:27 +0800

    uwsgi helloworld file

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#@app.route('/')
+#def hello_world():
+#    return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run() #port=8888,host='0.0.0.0'
+#if __name__ == '__main__':
+#    app.run() #port=8888,host='0.0.0.0'
+
+
+def application(env, start_response):
+    import pdb; pdb.set_trace()
+    start_response('200 OK', [('Content-Type','text/html')])
+    return "Hello World"
