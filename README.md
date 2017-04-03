d1ac783ddfe42adae9684b5684c2f8c7a65a4d26
vhy贺悦 <he.y@Ctrip.com>
2017-04-03 11:43:20 +0800

    v7

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SuperService
 各种无厘头的小功能
+
+
+# uwsgi --socket 0.0.0.0:80 --wsgi-file hello.py
+# 目前报错，各种EMPTY_RESPONSE
+# TODO: 怎么指定入口？
--- hello.py
+++ hello.py
@@ -9,31 +9,5 @@ def index():
 def hello_world():
     return 'Hello World!'
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/urlfor')
-def url():
-    return url_for('show_user_profile', username='abbc')
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
-
-
 if __name__ == '__main__':
-    app.run() #port=8888,host='0.0.0.0'
-
-
-#def application(env, start_response):
-#    import pdb; pdb.set_trace()
-#    start_response('200 OK', [('Content-Type','text/html')])
-#    return "Hello World"
+    app.run(host='0.0.0.0',port=8888)
