ff8108841275f7d14db08e7a7131a9a79901aa98
vhy贺悦 <he.y@Ctrip.com>
2017-04-05 18:22:09 +0800

    111

--- code.py
+++ code.py
@@ -1,14 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import web
 import view, config
 from view import render
 
-urls = ("/", "index")
+urls = ("/.*", "index")
 
 class index:
     def GET(self):
     return render.base(view.listing())
+
+
 if __name__ == "__main__":
     web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
     app = web.application(urls, globals())
     app.internalerror = web.debugerror
     app.run()
+
