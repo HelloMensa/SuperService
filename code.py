fae8a22ec7b21384507f4ebcff73f17ac0298a90
vhy贺悦 <he.y@Ctrip.com>
2017-04-05 18:06:54 +0800

    encode?

--- code.py
+++ code.py
@@ -2,16 +2,13 @@ import web
 import view, config
 from view import render
 
-urls = (
-    "/", "index"
-)
+urls = ("/", "index")
 
 class index:
     def GET(self):
-        return render.base(view.listing())
-
+    return render.base(view.listing())
 if __name__ == "__main__":
-	web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
     app = web.application(urls, globals())
     app.internalerror = web.debugerror
     app.run()
