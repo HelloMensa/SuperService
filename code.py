5ba88ce2e69274fb05841ec6eaab966b0a2dfb66
vhy贺悦 <he.y@Ctrip.com>
2017-04-05 17:58:18 +0800

    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)

--- code.py
+++ code.py
@@ -11,6 +11,7 @@ class index:
         return render.base(view.listing())
 
 if __name__ == "__main__":
+	web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
     app = web.application(urls, globals())
     app.internalerror = web.debugerror
     app.run()
